9383f08013acb1cddba04356a12343474f23802a
mmatheuslemos <matheusfelipelemos@hotmail.com>
2022-05-21 20:36:54 -0300

    Atualização 3

---
+++ Strings.js
@@ -0,0 +1,30 @@
+//Declarando strings.
+//String como um objeto
+var city = new String("Araguari"); 
+
+//Retorna o número de caracteres da string.
+var lccity = city.length 
+console.log(city.length);
+
+//Exemplo de quebra de linha
+var city2 = ("São\nPaulo"); // \n pula uma linha
+console.log(city2); 
+
+//Aparecerá a primeira letra da palavra da string.
+var city12 = new String("Araguari"); 
+console.log(city12[0]);
+console.log(city12.charAt(0)); 
+
+//Exemplo de concatenar variável string.
+var exemplo1 = "Hello";
+var exemplo2 = "World";
+console.log(exemplo1.concat(exemplo2)); 
+
+//Irá aperecer o tipo de variável no caso é um objeto.
+var city = new String("Araguari"); 
+var city2 = "Araguari"
+console.log(typeof(city)); 
+console.log(typeof(city2)); 
+
+
+
